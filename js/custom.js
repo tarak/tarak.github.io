6dac91831673b64f832859fa4bf77119c775933b
Tarak Blah <halbkarat@gmail.com>
2018-01-30 17:58:48 +0100

    Minor fixes. Closes #1.

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,7 @@
 $('#logo').click(function(){
-  console.log('test');
 	$('html, body').animate({scrollTop : -51},800);
 	return false;
 });
-// $(".page-scroll").on('click', function(e) {
-//   e.preventDefault();
-//   var hash = this.hash;
-//   var scrollPos = $(this.hash).offset().top - 51
-//   $('html, body').animate({
-//     scrollTop: scrollPos
-//   }, 750, function() {
-//     window.location.hash = hash;
-//   });
-// });
 $('a.page-scroll').bind('click', function(event) {
         var w = jQuery(window).width()
         var $anchor = $(this);
