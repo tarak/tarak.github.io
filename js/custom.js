319aa87c2cde7ee0d26d590ea96499fed976e0c5
Tarak Blah <halbkarat@gmail.com>
2018-01-30 17:34:53 +0100

    Update.

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,31 @@
-$(".page-scroll").on('click', function(e) {
-  e.preventDefault();
-  var hash = this.hash;
-  $('html, body').animate({
-    scrollTop: $(this.hash).offset().top - 56
-  }, 750, function() {
-    window.location.hash = hash;
-  });
+$('#logo').click(function(){
+  console.log('test');
+	$('html, body').animate({scrollTop : -51},800);
+	return false;
 });
+// $(".page-scroll").on('click', function(e) {
+//   e.preventDefault();
+//   var hash = this.hash;
+//   var scrollPos = $(this.hash).offset().top - 51
+//   $('html, body').animate({
+//     scrollTop: scrollPos
+//   }, 750, function() {
+//     window.location.hash = hash;
+//   });
+// });
+$('a.page-scroll').bind('click', function(event) {
+        var w = jQuery(window).width()
+        var $anchor = $(this);
+        if (w < 567) {
+          var scrollPos = ($($anchor.attr('href')).offset().top - 51);
+        } else {
+          var scrollPos = ($($anchor.attr('href')).offset().top);
+        }
+        $('html, body').stop().animate({
+            scrollTop: scrollPos
+        }, 1250);
+        event.preventDefault();
+    });
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
