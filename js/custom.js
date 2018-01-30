5b6d1f04610954241e881da10c87822026d7e8d0
Tarak Blah <halbkarat@gmail.com>
2018-01-30 18:07:26 +0100

    Some more scrolling magic code.

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,31 @@
-$('#logo').click(function(){
-	$('html, body').animate({scrollTop : -51},800);
-	return false;
+$('#logo').click(function() {
+  var $anchor = $(this);
+  var $width = jQuery(window).width();
+  if ($width < 567) {
+    var scrollPos = 0;
+  } else {
+    var scrollPos = -51;
+  }
+  $('html, body').animate({
+    scrollTop: scrollPos
+  }, 1250);
+  return false;
 });
 $('a.page-scroll').bind('click', function(event) {
-        var w = jQuery(window).width()
-        var $anchor = $(this);
-        if (w < 567) {
-          var scrollPos = ($($anchor.attr('href')).offset().top - 51);
-        } else {
-          var scrollPos = ($($anchor.attr('href')).offset().top);
-        }
-        $('html, body').stop().animate({
-            scrollTop: scrollPos
-        }, 1250);
-        event.preventDefault();
-    });
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+  var w = jQuery(window).width()
+  var $anchor = $(this);
+  if (w < 567) {
+    var scrollPos = ($($anchor.attr('href')).offset().top - 51);
+  } else {
+    var scrollPos = ($($anchor.attr('href')).offset().top);
+  }
+  $('html, body').stop().animate({
+    scrollTop: scrollPos
+  }, 1250);
+  event.preventDefault();
+});
+$('.navbar-nav>li>a').on('click', function() {
+  $('.navbar-collapse').collapse('hide');
 });
 wow = new WOW();
 wow.init();
